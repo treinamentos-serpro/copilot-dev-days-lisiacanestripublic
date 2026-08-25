3e870ef625d3dea7eacdd56a153f5c858710f854
Lisia Canestri <60950413+lisiacanestri@users.noreply.github.com>
2026-08-25 18:25:50 +0000

    Adicionar configuração do Checkstyle e ajustar importações nos arquivos Java

--- socops/src/test/java/com/socops/service/BoardAssemblerTests.java
+++ socops/src/test/java/com/socops/service/BoardAssemblerTests.java
@@ -1,15 +1,14 @@
 package com.socops.service;
 
-import com.socops.model.BingoCell;
-import com.socops.model.WinningStreak;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import com.socops.model.BingoCell;
+import com.socops.model.WinningStreak;
 
 import static org.junit.jupiter.api.Assertions.*;
 
